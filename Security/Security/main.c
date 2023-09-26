5d86f95de6192d96e72c236de14c6419b8eb87af
gaejuk <jejuvita01@gmail.com>
2023-09-26 14:33:45 +0900

    initialization

---
+++ Security/Security/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Security
+//
+//  Created by 김서현 on 9/25/23.
+//
+
+#include <stdio.h>
+#include <openssl/bn.h>
+
+int main(int argc, const char * argv[]) {
+    BIGNUM *test;
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
